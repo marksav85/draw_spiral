1cf8e23e5d0d0bdc8dee9c251b249ed54843d4c1
Mark Saville <mark.saville@outlook.com>
2020-11-13 11:21:35 +0100

    Create function to draw square shape

---
+++ draw_spiral.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 11:18:31 2020
+
+@author: marks
+"""
+import turtle
+
+def shape_square(ninja_turtle):
+    for i in range(1,5):
+        ninja_turtle.forward(100)
+        ninja_turtle.right(90)
