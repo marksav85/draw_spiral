853047e520c52fb69e4d39498eba169966885eb5
Mark Saville <mark.saville@outlook.com>
2020-11-16 16:02:04 +0100

    Create function to rotate squares in a spiral

--- draw_spiral.py
+++ draw_spiral.py
@@ -10,3 +10,15 @@ def shape_square(ninja_turtle):
     for i in range(1,5):
         ninja_turtle.forward(100)
         ninja_turtle.right(90)
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("white")
+    #Create turtle Raph - Draws a square
+    raph = turtle.Turtle()
+    raph.shape("turtle")
+    raph.color("yellow")
+    raph.speed()
+    for i in range(1,37):
+        draw_square(raph)
+        raph.right(10)
